45491d0c0ab51b0c22e91b0647780590887719ae
Vivek Sampara <ravensnowbird@gmail.com>
2018-01-29 18:50:07 +0800

    trying out heroku sort order with desc

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,14 +1,7 @@
 class Tweet < ApplicationRecord
 
-  default_scope { order(retweet_count: sort_order).limit(10) }
+  default_scope { order(retweet_count: :desc).limit(10) }
 
   validates :body, length: { maximum: 140 }
 
-  def self.sort_order
-    if Rails.env.production?
-      :asc
-    else
-      :desc
-    end
-  end
 end
