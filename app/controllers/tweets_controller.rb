6fd0f8024ccfb82c0a00058ba1d2901f6a47bb0d
Vivek Sampara <ravensnowbird@gmail.com>
2018-01-29 18:47:49 +0800

    Added seamless sorting after creating , retweeting and deleting

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,8 +28,8 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to tweets_url, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
+        @tweets = Tweet.all
+        format.js
       else
         format.html { render :new }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
@@ -42,8 +42,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
+        @tweets = Tweet.all
         format.js
       else
         format.html { render :edit }
@@ -57,8 +56,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
+      @tweets = Tweet.all
       format.js
     end
   end
